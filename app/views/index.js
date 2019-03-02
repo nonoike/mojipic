57c9d32fc34ce86a57cda901767253ea66b61f01
nonoike <ij4nu8d4fw@gmail.com>
2019-03-02 21:39:41 +0900

    [add] implements html and js

--- app/views/index.js
+++ app/views/index.js
@@ -3,3 +3,9 @@ import 'materialize-css';
 import Dropzone from 'dropzone';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Materializedの設定
+$(function () {
+  // Materialized Menu
+  $('.button-collapse').sideNav();
+}); // end of document ready
