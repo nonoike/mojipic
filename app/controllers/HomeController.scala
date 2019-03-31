ff4d8b97c4346a4ed4dd43c95aebae5b5f4d49f4
nonoike <ij4nu8d4fw@gmail.com>
2019-03-31 15:49:35 +0900

    [update] implement login and logout in view

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,27 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
+import play.api.cache.SyncCacheApi
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(
+                                val cache: SyncCacheApi,
+                                cc: ControllerComponents
+                              ) extends TwitterLoginController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index() = TwitterLoginAction { implicit request: TwitterLoginRequest[AnyContent] =>
+    Ok(views.html.index(request.accessToken))
   }
 }
